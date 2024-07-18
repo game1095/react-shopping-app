d0bb3cbb97c1b6fb8b73132318938b390bf69e82
game1095 <taweesak.in@gmail.com>
2024-07-18 20:04:27 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <header>
+      <p>Shopping Application</p>
+      <p>จำนวนสินค้าในตะกร้า : 0</p>
+    </header>
+  );
+}
